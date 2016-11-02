ca140bf6a6d8ccdf301b88cbad8195decafb7008
Praneeth Denduluri <praneeth@filtrest.com>
2016-11-02 16:57:50 -0400

    Final code

--- data_base.py
+++ data_base.py
@@ -1,3 +1,12 @@
+#
+#   Author  : Varunsrivathsa Venkatesha
+#   Date    : August 2016
+#
+#   This file is to create a class which maps to a database
+#
+#
+
+
 import os
 import sys
 
@@ -9,13 +18,18 @@ from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
+
 class Items(Base):
+        """
+        This calss is to create table in database
+        Each attribute will be a column in the database
+        """
         __tablename__ = 'items'
 
         id = Column(Integer, primary_key=True)
         name = Column(String(40))
         picture = Column(String(100))
-        price = Column(Integer)
+        price = Column(String(40))
         type_item = Column(String(40))
         description = Column(String(200))
         brand_name = Column(String(30))
@@ -24,29 +38,13 @@ engine = create_engine('sqlite:///item_list.db')
 Base.metadata.bind = engine
 DBsession = sessionmaker(bind=engine)
 session = DBsession()
+'''
+try:
+    num_rows_deleted = session.query(Items).delete()
+    session.commit()
+except:
+    session.rollback()
+'''
 
-tshirt1 = Items(name = "Avengers Tshirt", picture = "http://ep.yimg.com/ay/stylinonline/avengers-black-widow-assemble-logo-juniors-t-shirt-11.jpg", price = 25, type_item = "Clothes", description = "Avengers 1 logo", brand_name = "levis")
-
-tshirt2 = Items(name = "Batman Tshirt", picture="http://images.sportsdirect.com/images/imgzoom/59/59839703_xxl.jpg", price= 26, type_item = "Clothes", description = "dark knight rises", brand_name = "adidas")
-
-mobile1 = Items(name = "S6 Edge", picture = "http://newdaypost.com/wp-content/uploads/2015/07/Galaxy-S6-Edge-3.jpg", price = 700, type_item = "Electronics", description = "New Unlocked", brand_name = "samsung")
-
-mobile2 = Items(name = "Nexus 4", picture = "http://www.lg.com/uk/images/lg-mobile-phones/e960/gallery/medium06.jpg", price = 280, type_item = "Electroincs", description = "New Unlocked 2008 edition", brand_name = "LG")
-
-book1 = Items(name = "Wings of Fire", picture = "https://upload.wikimedia.org/wikipedia/en/3/3a/Wings_of_Fire_by_A_P_J_Abdul_Kalam_Book_Cover.jpg", price = 70, type_item = "Books", description = "New Edition", brand_name = "MES Publishers")
-
-book2 = Items(name = "Game of Thrones", picture = "http://i.lv3.hbo.com/custom-assets/img/free-episodes/game-of-thrones-1349.jpg", price = 80, type_item = "Books", description  = "Second Book", brand_name = "GOT")
-
-session.add(tshirt1)
-session.add(tshirt2)
-session.add(mobile1)
-session.add(mobile2)
-session.add(book1)
-session.add(book2)
-
-session.commit()
-c = session.query(Items).all()
-for x in c:
-        print(x.name)
 
 Base.metadata.create_all(engine)
---
+++ templates/tasksched.py
@@ -0,0 +1,12 @@
+import Queue
+import threading
+import time
+
+exitFlag = 0
+
+enum Status=['QUEUED',"RUNNING","FINISHED"]
+
+Queue q = New Queu
+TaskScheduler(?)
+    StartTask(?)
+        q
